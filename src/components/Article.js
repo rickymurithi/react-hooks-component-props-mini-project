07d7b72956382facba80021170386d0699799cd0
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-10-02 13:46:47 +0300

    add files

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,16 +1,16 @@
-import React from 'react'
+
 
 function Article({title, date = "Feb 28, 1990" preview, minutes}) {
 
     let takes =""
     let i=0;
     if (minutes > 30) {
-        while (i<minutes/10) {takes =takes + "";
+        while (i<minutes/10) { takes =takes + "🍱";
     i++;
     }
     } else{
         while(i< minutes/5) {
-            takes =takes +"";
+            takes =takes +"☕️ ";
             i++;
         }
     }
@@ -18,7 +18,7 @@ function Article({title, date = "Feb 28, 1990" preview, minutes}) {
   return (
     <article>
         <h3>{title}</h3>
-        <small>{date} {takes} {minutes} minutes min read</small>
+        <small>{date} {takes} {minutes} minutes to read</small>
         <p>{preview}</p>
     </article>
   )
